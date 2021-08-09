a81f077e1ab9d4ae9e8eaa05e6ba1ed1f62cd710
Brandon <43820089+BManNap@users.noreply.github.com>
2021-08-08 19:06:02 -0500

    Update newYorkTimes.py

--- newYorkTimes.py
+++ newYorkTimes.py
@@ -1,24 +1,43 @@
+#Fuck you imports go up here
 import requests
 from bs4 import BeautifulSoup
-url = input("input url: ")
-
-r=requests.get(url)
-sourceCode=r.text
 
 #asks if they want to see the source code
-while True:
-    show = input("show source code? y/n: ").lower()
-    print(show)
-    if show == 'y':
-        print(sourceCode) #debug reasons
-        break
-    elif show == 'n':
-        break
+def showCode(sourceCode):
+    while True:
+        show = input("show source code? y/n: ").lower()
+        print(show)
+        if show == 'y':
+            print(sourceCode) #debug reasons
+            break
+        elif show == 'n':
+            break
+        else:
+            print("Please input y or n")
+
+#Deals with what tag you're looking for
+def whatYourLookingFor(sourceCode):
+    soup = BeautifulSoup(sourceCode, features="html.parser")
+    print("\nType none if none else p is a good tag (It's case sensitive)")
+    tag = input('Input tag looking for: ')
+    if tag == 'p':
+        for para in soup.find_all("p"):
+            print(para.get_text())
+    elif tag == 'none':
+        exit
     else:
-        print("Please input y or n")
+        for para in soup.find_all(tag):
+            print(para)
+        #page = soup.find_all(tag)
+        #print(page)
 
-#soup code
-soup = BeautifulSoup(sourceCode)
-page = soup.find_all(input('Input tag looking for (just do P if nothing else):'))
-print(page)
+#main
+def main():
+    url = input("input url: ")
+    r=requests.get(url)
+    sourceCode=r.text
 
+    showCode(sourceCode)
+    whatYourLookingFor(sourceCode)
+
+main()
